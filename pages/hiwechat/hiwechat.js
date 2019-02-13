fd5fb205cc153d64dcba8cf81ed72fbf84cd5d30
ingangi <huahui_c@hotmail.com>
2019-02-13 16:35:42 +0800

    ios date null problem

--- pages/hiwechat/hiwechat.js
+++ pages/hiwechat/hiwechat.js
@@ -28,11 +28,16 @@ Page({
   updatelistData(res) {
     console.log("updatelistData", res.data)
 
-    var today = new Date().setHours(0, 0, 0, 0)
+    var today = new Date()//.setHours(0, 0, 0, 0)
+    today.setHours(0, 0, 0, 0)
     for (var i in res.data) {
       res.data[i].CostSeconds = this.formatSeconds(res.data[i].CostSeconds)
 
-      var t = new Date(res.data[i].StopTime)
+      var time = res.data[i].StartTime
+      time = time.replace(/-/g, ':').replace(' ', ':');
+      time = time.split(':');
+      var t = new Date(time[0], (time[1] - 1), time[2], time[3], time[4], time[5]);
+      //console.log("today:", today.toString(), "t:", t.toString())
       if (today > t) {
         res.data[i].State = 99 // 非今日数据
       }
